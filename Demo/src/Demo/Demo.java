c75aa03f59765eaa04d84ade62e0efc52248503f
fa12-bse-155 <fa12-bse-155@LAB7B-12.cns.ciit>
2016-04-25 18:07:15 +0500

    Your SECOND COMMIT

--- Demo/src/Demo/Demo.java
+++ Demo/src/Demo/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I am Mubashar Islam");
+		System.out.println(" JADOON");
 
 	}
 
